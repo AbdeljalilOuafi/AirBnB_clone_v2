929faa4f11a50ee0349166058b1d745accb5ed5f
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-10 22:46:18 +0100

    1-pack_web_static.py_style

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,17 +4,17 @@ from fabric.api import local
 import tarfile
 from datetime import datetime
 
+
 def do_pack():
     """compress a webstatic folder into a .tgz"""
     local("mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    #-c : creates a new archive
-    #-v : verbose
-    #-z : compression
-    #-f : file
+    # -c : creates a new archive
+    # -v : verbose
+    # -z : compression
+    # -f : file
     result = local(f"tar -cvzf versions/web_static{date}.tgz web_static")
     if result.return_code == 0:
         return f"versions/web_static{date}.tgz"
     else:
         return None
-
