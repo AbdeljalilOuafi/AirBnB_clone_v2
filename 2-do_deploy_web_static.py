af2ab23a429c3a5dd69c8be15f2e3c2f4d7067d2
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-11 14:00:49 +0100

    feat: Check is current exists before deleting it to avoid erros

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """a fabric script that compressed"""
 from fabric.api import local, put, run, env, task
+from fabric.contrib.files import exists
 from datetime import datetime
 import os
 
@@ -14,10 +15,7 @@ def do_pack():
     """compress a webstatic folder into a .tgz"""
     local("mkdir -p versions")
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    # -c : creates a new archive
-    # -v : verbose
-    # -z : compression
-    # -f : file
+    # -c: creates a new archive, -v: verbose, -z: compression, -f: file
     result = local(f"tar -cvzf versions/web_static{date}.tgz web_static")
     if result.return_code == 0:
         return f"versions/web_static{date}.tgz"
@@ -37,17 +35,16 @@ def do_deploy(archive_path):
         current = "/data/web_static/current"
 
         put(archive_path, '/tmp/')
-        run("sudo rm -rf {}{}/".format(releases_path, file_name_no_ext))
-        # create directory if they don't exists on a new machine, skip
-        # if already exists
         run(f"sudo mkdir -p {releases_path}")
         run(f"sudo tar -xf /tmp/{file_name} -C {releases_path} && sudo mv \
             {releases_path}web_static {releases_path}{file_name_no_ext}")
         run(f"rm '/tmp/{file_name}'")
-        run(f"sudo rm {current}")
+        if exists(current):
+            run(f"sudo rm {current}")
+        else:
+            pass
         run(f"sudo ln -s {releases_path}{file_name_no_ext} {current}")
         print("New version deployed!")
         return True
     except Exception:
         return False
-
