73c9f3e9bd1a69aa51cb7e8b8259dd9d327f7804
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-11 12:15:59 +0100

    feat: running commands using sudo in case the directories are not writable by the user

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,60 +25,29 @@ def do_pack():
         return None
 
 
-# @task
-# def do_deploy(archive_path):
-#     """ deploy an archive to your web servers """
-#     if not os.path.exists(archive_path):
-#         return False
-#     try:
-#         releases_path = "/data/web_static/releases/"
-#         file_name = os.path.basename(archive_path)
-#         file_name_no_ext = file_name.split(".")[0]
-#         current = "/data/web_static/current"
-
-#         put(archive_path, '/tmp/')
-#         run("rm -rf {}{}/".format(releases_path, file_name_no_ext))
-#         # create directory if they don't exists on a new machine, skip
-#         # if already exists
-#         run(f"mkdir -p {releases_path}")
-#         run(f"tar -xf /tmp/{file_name} -C {releases_path} && mv \
-#             {releases_path}web_static {releases_path}{file_name_no_ext}")
-#         run(f"rm '/tmp/{file_name}'")
-#         run(f"rm {current}")
-#         run(f"ln -s {releases_path}{file_name_no_ext} {current}")
-#         print("New version deployed!")
-#         return True
-#     except Exception:
-#         return False
-
+@task
 def do_deploy(archive_path):
-    """deploy package to remote server
-    Arguments:
-        archive_path: path to archive to deploy
-    """
-    if not archive_path or not os.path.exists(archive_path):
+    """ deploy an archive to your web servers """
+    if not os.path.exists(archive_path):
         return False
-    put(archive_path, '/tmp')
-    ar_name = archive_path[archive_path.find("/") + 1: -4]
     try:
-        run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
-        run('rm /tmp/{}.tgz'.format(ar_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
-        run('rm -rf /data/web_static/releases/{}/web_static'.format(
-            ar_name
-        ))
-        run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(
-            ar_name
-        ))
+        releases_path = "/data/web_static/releases/"
+        file_name = os.path.basename(archive_path)
+        file_name_no_ext = file_name.split(".")[0]
+        current = "/data/web_static/current"
+
+        put(archive_path, '/tmp/')
+        run("sudo rm -rf {}{}/".format(releases_path, file_name_no_ext))
+        # create directory if they don't exists on a new machine, skip
+        # if already exists
+        run(f"sudo mkdir -p {releases_path}")
+        run(f"sudo tar -xf /tmp/{file_name} -C {releases_path} && sudo mv \
+            {releases_path}web_static {releases_path}{file_name_no_ext}")
+        run(f"rm '/tmp/{file_name}'")
+        run(f"sudo rm {current}")
+        run(f"sudo ln -s {releases_path}{file_name_no_ext} {current}")
         print("New version deployed!")
         return True
-    except:
+    except Exception:
         return False
+
