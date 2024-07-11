8d176f7ab73859a8fab820ceef43ecf336c656ab
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-11 11:35:03 +0100

    test

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -25,25 +25,53 @@ def do_pack():
         return None
 
 
+# @task
+# def do_deploy(archive_path):
+#     """ deploy an archive to your web servers """
+#     if not os.path.exists(archive_path):
+#         return False
+#     try:
+#         releases_path = "/data/web_static/releases/"
+#         file_name = os.path.basename(archive_path)
+#         file_name_no_ext = file_name.split(".")[0]
+#         current = "/data/web_static/current"
+
+#         put(archive_path, '/tmp/')
+#         run("rm -rf {}{}/".format(releases_path, file_name_no_ext))
+#         # create directory if they don't exists on the new machine
+#         run(f"mkdir -p {releases_path}")
+#         run(f"tar -xf /tmp/{file_name} -C {releases_path} && mv \
+#             {releases_path}web_static {releases_path}{file_name_no_ext}")
+#         run(f"rm '/tmp/{file_name}'")
+#         run(f"rm {current}")
+#         run(f"ln -s {releases_path}{file_name_no_ext} {current}")
+#         print("New version deployed!")
+#         return True
+#     except Exception:
+#         return False
+
 @task
 def do_deploy(archive_path):
-    """ deploy an archive to your web servers """
-    if not os.path.exists(archive_path):
-        return False
+    """ method doc
+        fab -f 2-do_deploy_web_static.py do_deploy:
+        archive_path=versions/web_static_20231004201306.tgz
+        -i ~/.ssh/id_rsa -u ubuntu
+    """
     try:
-        releases_path = "/data/web_static/releases/"
-        file_name = os.path.basename(archive_path)
-        file_name_no_ext = file_name.split(".")[0]
-        current = "/data/web_static/current"
-
-        put(archive_path, '/tmp/')
-        run("rm -rf {}{}/".format(releases_path, file_name_no_ext))
-        run(f"mkdir -p {releases_path}")
-        run(f"tar -xf /tmp/{file_name} -C {releases_path} && mv \
-            {releases_path}web_static {releases_path}{file_name_no_ext}")
-        run(f"rm '/tmp/{file_name}'")
-        run(f"rm {current}")
-        run(f"ln -s {releases_path}{file_name_no_ext} {current}")
+        if not os.path.exists(archive_path):
+            return False
+        fn_with_ext = os.path.basename(archive_path)
+        fn_no_ext, ext = os.path.splitext(fn_with_ext)
+        dpath = "/data/web_static/releases/"
+        put(archive_path, "/tmp/")
+        run("rm -rf {}{}/".format(dpath, fn_no_ext))
+        run("mkdir -p {}{}/".format(dpath, fn_no_ext))
+        run("tar -xzf /tmp/{} -C {}{}/".format(fn_with_ext, dpath, fn_no_ext))
+        run("rm /tmp/{}".format(fn_with_ext))
+        run("mv {0}{1}/web_static/* {0}{1}/".format(dpath, fn_no_ext))
+        run("rm -rf {}{}/web_static".format(dpath, fn_no_ext))
+        run("rm -rf /data/web_static/current")
+        run("ln -s {}{}/ /data/web_static/current".format(dpath, fn_no_ext))
         print("New version deployed!")
         return True
     except Exception:
