ad55995fab10d8660162ae8167b5364bc50d58cc
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-13 16:11:33 +0100

    0-setup_web_static.sh_update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,23 +1,24 @@
 #!/usr/bin/env bash
-#sets up web servers for the deployment
+#A Bash script that sets up the web servers for the deployment
+sudo apt-get -y update
+sudo apt-get -y upgrade
 sudo apt-get -y install nginx
-sudo mkdir -p /data/
-sudo mkdir -p /data/web_static/
-sudo mkdir -p /data/web_static/releases/
-sudo mkdir -p /data/web_static/shared/
-sudo mkdir -p /data/web_static/releases/test/
-sudo touch /data/web_static/releases/test/index.html
-sudo bash -c 'cat <<EOF > /data/web_static/releases/test/index.html
-<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>
-EOF'
 
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+fake_HTML="<!DOCTYPE html>
+<html lang=\"en\">
+<head></head>
+<body>
+    <h1>Holberton School</h1>
+</body>
+</html>"
+
+echo "$fake_HTML" | sudo tee /data/web_static/releases/test/index.html
+
+sudo rm -rf /data/web_static/current
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
-sudo sed -i '/server_name _;/a\\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}' /etc/nginx/sites-available/default
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
 sudo service nginx restart
