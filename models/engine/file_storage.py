a62f2330310659b9693bcead474805b5e6ac85e0
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-26 16:50:26 +0100

    style

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -75,6 +75,7 @@ class FileStorage:
                     self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
+
     def close(self):
         """close the session"""
         self.reload()
