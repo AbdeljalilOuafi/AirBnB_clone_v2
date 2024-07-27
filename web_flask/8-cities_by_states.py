acb2b7fbea5e8d0d35753feb88c0ae58d50dfe5c
Abdeljalil Ouafi <abdeljalilouafi44@gmail.com>
2024-07-27 12:06:07 +0100

    Hmmm

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,27 +1,25 @@
 #!/usr/bin/python3
+"""
+starts a Flask web application
+"""
 
-""" module doc """
 from flask import Flask, render_template
+from models import *
 from models import storage
-from models.state import State
-
 app = Flask(__name__)
 
 
 @app.route('/cities_by_states', strict_slashes=False)
-def states_list_and_cities():
-    """ render all states and their corresponding
-        cities from the database
-    """
-    states = storage.all(State).values()
+def city_by_state():
+    """display the states and cities listed in alphabetical order"""
+    states = storage.all("State").values()
     return render_template('8-cities_by_states.html', states=states)
 
 
 @app.teardown_appcontext
-def close(error):
-    """ clean up and close db connection """
+def close_dataB(exception):
+    """closes the storage on teardown"""
     storage.close()
 
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='5000')
